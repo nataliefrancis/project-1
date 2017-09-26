d257dd35070fc084dc4ac0d9b9476af184a29423
nataliefrancis <natdfrancis@gmail.com>
2017-09-26 12:22:23 -0600

    Adds buttons to checkWinner and a restart function

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 console.log("ready");
 
-let position = 0, quiz, quiz_status, question, plyrChoice, choices, ansA, ansB, ansC;
-var correct = 0
+let position = 0, quiz, question, plyrChoice, choices, ansA, ansB, ansC;
+let correct = 0
 let playerOne = 0; //Player one score
 let playerTwo = 0; //Player two score
 let turn = 0 //This is used to notify when it is player 2's turn
+
+quiz = document.getElementById('quiz'); //#quiz div
 
 ///////////// QUESTIONS AND ANSWERS //////////////////
 let questions = [
@@ -24,11 +26,15 @@ let questions = [
 
 /////////////// QUESTION GENERATOR ////////////////////
 function renderQ() {
-	quiz = document.getElementById('quiz'); //#quiz div
 	if(turn == 5) {
 			turn++;
-			playerOne = correct
+			playerOne = correct;    //Logs the score for player one
 			alert("It is player two's turn now"); //switch players
+	}
+	if(turn == 11) {
+		playerTwo = correct - playerOne;
+		checkWinner();
+		return false;
 	}
 	question = questions[position][0];
 	ansA = questions[position][1];
@@ -46,15 +52,40 @@ function checkAnswer() {
 	choices = document.getElementsByName('choices');
 	for(let i = 0; i < choices.length; i++) {
 		if (choices[i].checked) {
-			plyrChoice = choices[i].value; //stores the value of their choice
+			plyrChoice = choices[i].value;             //stores the value of their choice
 		}
 	};
-	if (plyrChoice == questions[position][4]) { //Compares the value of their choice to the correct answer
-		correct++; //Stores a point for what they got right
+	if (plyrChoice == questions[position][4]) {   //Compares the value of their choice to the correct answer
+		correct++;             //Stores a point for what they got right
 	};
-	turn++ //logs how many questions have been asked
-	position= Math.floor((Math.random() * 7) + 0); //randomizes the question number
+	turn++             //logs how many questions have been asked
+	position= Math.floor((Math.random() * 7) + 0);          //randomizes the question number
 	renderQ();
 };
+
+
+////////////// Checks for a winner by comparing the scores ///////////////////
+function checkWinner() {
+	if(playerOne > playerTwo) {
+		quiz.innerHTML = "<h2>Player One wins $1,000,000!!!</h2>";
+		quiz.innerHTML += "<button class='next-button' onclick=restart()>Play again</button>";
+	} else if(playerOne < playerTwo) {
+		quiz.innerHTML = "<h2>Player Two wins $1,000,000!!!</h2>";
+		quiz.innerHTML += "<button class='next-button' onclick=restart()>Play again</button>";
+	} else {
+		quiz.innerHTML = "<h2>You tied!</h2>";
+		quiz.innerHTML += "<button class='next-button' onclick=restart()>Play again</button>";
+	}
+};
+
+///////// Restarts the game //////////////
+function restart() {
+	correct = 0;
+	playerOne = 0;
+	playerTwo = 0;
+	turn = 0;
+	position= Math.floor((Math.random() * 7) + 0);
+	renderQ();
+}
 
 window.addEventListener("load", renderQ(), false);
